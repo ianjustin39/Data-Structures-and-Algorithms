5bbb0a015956c66cb1e5a01b257b3fc2e85e4f53
ian <ianjustin39@gmail.com>
2021-03-08 21:45:36 +0800

    add binary search practice 01 and 02

--- Algorithms/Binary Search/02_binary_search_recursive_Q.py
+++ Algorithms/Binary Search/02_binary_search_recursive_Q.py
@@ -1,18 +1,22 @@
-def binary_search(array, target):
+def binary_search_recursive(array, target):
+    return binary_search_recursive_soln(array, target, 0, len(array) - 1)
+
+
+def binary_search_recursive_soln(array, target, start_index, end_index):
     # your code
     pass
 
 
 def test_function(test_case):
-    answer = binary_search(test_case[0], test_case[1])
+    answer = binary_search_recursive(test_case[0], test_case[1])
     if answer == test_case[2]:
         print("Pass!")
     else:
         print("Fail!")
 
 
-array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = 6
-index = 6
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 4
+index = 4
 test_case = [array, target, index]
 test_function(test_case)
